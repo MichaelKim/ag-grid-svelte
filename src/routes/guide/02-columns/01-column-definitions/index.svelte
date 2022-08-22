a7a6f60681242483fe39c7e1ba0b77f2c9771450
Michael Kim <micklepickle98@gmail.com>
2022-08-22 01:23:17 -0500

    add some tests for nested row data

--- src/routes/guide/02-columns/01-column-definitions/index.svelte
+++ src/routes/guide/02-columns/01-column-definitions/index.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import Definitions from './definitions.svelte';
   import Ids from './ids.svelte';
+  import NestedRowData from './nested-row-data.svelte';
   import Types from './types.svelte';
 </script>
 
 <Definitions />
+<NestedRowData />
 <Types />
 <Ids />
--- tests/examples/column-definitions/definitions.spec.ts
+++ tests/examples/column-definitions/definitions.spec.ts
@@ -1,16 +1,32 @@
-import { expect, test } from '@playwright/test';
+import { expect, test, type Page } from '@playwright/test';
+
+function getGrid(page: Page, index: number) {
+  return page.locator(`div.ag-theme-alpine:nth-of-type(${index})`);
+}
 
 test.describe('Column Definitions', () => {
-  test('Page loads', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('/guide/column-definitions');
+  });
+
+  test('Page loads', async ({ page }) => {
     expect(page).toHaveTitle('Svelte AG Grid - Column Definitions');
     expect(await page.textContent('h1')).toBe('Svelte AG Grid - Column Definitions');
   });
 
   test('Column Definitions', async ({ page }) => {
-    await page.goto('/guide/column-definitions');
-    const grid = page.locator('div.ag-theme-alpine').first();
+    const grid = getGrid(page, 1);
     const headers = grid.locator('span.ag-header-cell-text');
     await expect(headers).toContainText(['Athlete', 'Gold', 'Age']);
+  });
+
+  test('Nested Row Data', async ({ page }) => {
+    const grid = getGrid(page, 2);
+    const golds = [8, 6, 4, 1, 2, 1, 4, 2, 3, 2, 3, 2, 1, 1, 3, 2, 1, 2, 2, 1];
+
+    for (let i = 0; i < golds.length; i++) {
+      const cell = grid.locator(`div[row-id="${i}"] div[col-id="medals.gold"]`);
+      await expect(cell).toContainText(golds[i].toString());
+    }
   });
 });
