53a07c0db2dce405039d0e02cab1464b32a78d40
Michael Kim <micklepickle98@gmail.com>
2023-01-20 20:20:32 -0500

    docs: add syntax highlighting

---
+++ src/components/Highlight.svelte
@@ -0,0 +1,66 @@
+<script lang="ts" context="module">
+  import hljs from 'highlight.js/lib/core';
+  import css from 'highlight.js/lib/languages/css';
+  import plaintext from 'highlight.js/lib/languages/plaintext';
+  import typescript from 'highlight.js/lib/languages/typescript';
+  import xml from 'highlight.js/lib/languages/xml';
+  import 'highlight.js/styles/github.css';
+
+  hljs.registerLanguage('xml', xml);
+  hljs.registerLanguage('typescript', typescript);
+  hljs.registerLanguage('css', css);
+  hljs.registerLanguage('plaintext', plaintext);
+  hljs.registerLanguage('svelte', (hljs) => ({
+    subLanguage: 'xml',
+    contains: [
+      hljs.COMMENT('<!--', '-->', { relevance: 10 }),
+      {
+        begin: /^(\s*)(<script(\s*context="module")?>)/gm,
+        end: /^(\s*)(<\/script>)/gm,
+        subLanguage: 'typescript',
+        excludeBegin: true,
+        excludeEnd: true,
+        contains: [
+          {
+            begin: /^(\s*)(\$:)/gm,
+            end: /(\s*)/gm,
+            className: 'keyword'
+          }
+        ]
+      },
+      {
+        begin: /^(\s*)(<st\yle.*>)/gm,
+        end: /^(\s*)(<\/style>)/gm,
+        subLanguage: 'css',
+        excludeBegin: true,
+        excludeEnd: true
+      },
+      {
+        begin: /\{/gm,
+        end: /\}/gm,
+        subLanguage: 'typescript',
+        contains: [
+          {
+            begin: /[\{]/,
+            end: /[\}]/,
+            skip: true
+          },
+          {
+            begin: /([#:\/@])(if|else|each|await|then|catch|debug|const|html)/gm,
+            className: 'keyword',
+            relevance: 10
+          }
+        ]
+      }
+    ]
+  }));
+</script>
+
+<script lang="ts">
+  export let code: string;
+  export let lang = 'svelte';
+
+  $: highlighted = hljs.highlight(code, { language: lang }).value;
+</script>
+
+<pre><code class="hljs">{@html highlighted}</code></pre>
--- src/routes/guide/01-getting-started/01-overview/index.svelte
+++ src/routes/guide/01-getting-started/01-overview/index.svelte
@@ -1,12 +1,12 @@
+<script lang="ts">
+  import Highlight from '../../../../components/Highlight.svelte';
+</script>
+
 <h2>Getting Started</h2>
 
 <p>AG Grid Svelte is designed to provide a seamless AG Grid integration to any Svelte project.</p>
 
-<code>
-  <pre>
-  npm install ag-grid-svelte ag-grid-community
-</pre>
-</code>
+<Highlight code="npm install ag-grid-svelte ag-grid-community" lang="plaintext" />
 
 <h2>Usage</h2>
 
@@ -31,59 +31,51 @@
 
 <p>Before loading the grid, import the relevant CSS files:</p>
 
-<code>
-  <pre>
-  import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
-  import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
-</pre>
-</code>
+<Highlight
+  code={`import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS`}
+  lang="typescript"
+/>
 
 <p>Then, apply the chosen theme by adding the corresponding class to the parent container:</p>
 
-<code>
-  <pre>
-  {`<div className="ag-theme-alpine" style:width="500px" style:height="500px">
-    <AGGridSvelte ... />
-  </div>`}</pre>
-</code>
+<Highlight
+  code={`<div class="ag-theme-alpine" style:width="500px" style:height="500px">
+  <AGGridSvelte ... />
+</div>`}
+/>
 
 <h2>Accessing APIs</h2>
 
 <p>The Grid API and Column API objects can be accessed via prop bindings:</p>
 
-<code>
-  <pre>
-  {'<'}{`script>
-    let api, columnApi;
+<Highlight
+  code={`${'<'}script>
+  let api, columnApi;
 
-    const onClick = () => {
-      api.deselectAll();
-    };
-  </script>
-  
-  <AGGridSvelte
-    bind:api={api}
-    bind:columnApi={columnApi}
-  />`}
-</pre>
-</code>
+  const onClick = () => {
+    api.deselectAll();
+  };
+</script>
+
+<AGGridSvelte bind:api={api} bind:columnApi={columnApi} />
+`}
+/>
 
 <p>Alternatively, the onGridReady event will provide the APIs as well</p>
 
-<code>
-  <pre>
-  {'<'}{`script>
-    let api, columnApi;
+<Highlight
+  code={`${'<'}script>
+  let api, columnApi;
 
-    const onGridReady = (event) => {
-      api = event.api;
-      columnApi = event.columnApi;
-    };
-  </script>
-  
-  <AGGridSvelte {onGridReady} />`}
-</pre>
-</code>
+  const onGridReady = (event) => {
+    api = event.api;
+    columnApi = event.columnApi;
+  };
+</script>
+
+<AGGridSvelte {onGridReady} />`}
+/>
 
 <h2>Styling</h2>
 
@@ -92,22 +84,20 @@
   targeting inner class names:
 </p>
 
-<code>
-  <pre>
-  {`<div style:height="500px" class="ag-theme-alpine">
-    <AgGridSvelte {rowData} {columnDefs} {defaultColDef} {onGridReady} />
-  </div>
-  
-  <`}{`style>
-    .ag-theme-alpine {
-      --ag-background-color: #ddd;
-    }
-    .ag-theme-alpine :global(.ag-header-cell-label) {
-      font-style: italic;
-    }
-  </style>`}
-</pre>
-</code>
+<Highlight
+  code={`<div style:height="500px" class="ag-theme-alpine">
+  <AgGridSvelte {rowData} {columnDefs} {defaultColDef} {onGridReady} />
+</div>
+
+${'<'}style>
+  .ag-theme-alpine {
+    --ag-background-color: #ddd;
+  }
+  .ag-theme-alpine :global(.ag-header-cell-label) {
+    font-style: italic;
+  }
+</style>`}
+/>
 
 <h2>Enterprise</h2>
 
@@ -120,24 +110,22 @@
   script:
 </p>
 
-<code>
-  <pre>
-  {`<`}{`script context="module">
-    // Will get called only once when this module is loaded
-    import 'ag-grid-enterprise';
-  </script>`}</pre>
-</code>
+<Highlight
+  code={`${'<'}script context="module">
+  // Will get called only once when this module is loaded
+  import 'ag-grid-enterprise';
+</script>`}
+/>
 
 <p>You can set a license key in a similar way:</p>
 
-<code>
-  <pre>
-  {`<`}{`script context="module">
-    import { LicenseManager } from 'ag-grid-enterprise';
+<Highlight
+  code={`${'<'}script context="module">
+  import { LicenseManager } from 'ag-grid-enterprise';
 
-    LicenseManager.setLicenseKey("your license key");
-  </script>`}</pre>
-</code>
+  LicenseManager.setLicenseKey("your license key");
+</script>`}
+/>
 
 <p>
   For Vite / SvelteKit users, if you are getting a warning about the enterprise package missing, it
@@ -161,5 +149,5 @@
   >
     <code>optimize.include</code>
   </a>
-   in your Vite config.
+  in your Vite config.
 </p>
