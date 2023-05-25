f37798120df25a109ff7126bffd78f573099139c
Michael Kim <micklepickle98@gmail.com>
2023-05-25 16:54:34 -0400

    chore(docs): fix dynamic header heights example

--- src/routes/guide/02-columns/04-column-headers/dynamic-header-heights.svelte
+++ src/routes/guide/02-columns/04-column-headers/dynamic-header-heights.svelte
@@ -5,10 +5,6 @@
   import 'ag-grid-community/styles/ag-theme-alpine.css';
   import type { IOlympicData } from '../../types';
 
-  function setIdText(id: string, value: string | number | undefined) {
-    document.getElementById(id)!.innerHTML = value == undefined ? 'undefined' : value + '';
-  }
-
   const columnDefs = [
     {
       headerName: 'Athlete Details',
@@ -87,43 +83,46 @@
   let columnApi: ColumnApi;
   let api: GridApi<IOlympicData>;
 
+  let pivotStatus = false;
+  let headerHeight: number | undefined;
+  let groupHeaderHeight: number | undefined;
+  let floatingFiltersHeight: number | undefined;
+  let pivotGroupHeaderHeight: number | undefined;
+  let pivotHeaderHeight: number | undefined;
+
   const setPivotOn = () => {
-    document.querySelector('#requiresPivot')!.className = '';
-    document.querySelector('#requiresNotPivot')!.className = 'hidden';
     columnApi.setPivotMode(true);
-    setIdText('pivot', 'on');
+    pivotStatus = true;
   };
 
   const setPivotOff = () => {
-    document.querySelector('#requiresPivot')!.className = 'hidden';
-    document.querySelector('#requiresNotPivot')!.className = '';
     columnApi.setPivotMode(false);
-    setIdText('pivot', 'off');
+    pivotStatus = false;
   };
 
   const setHeaderHeight = (value?: number) => {
     api.setHeaderHeight(value);
-    setIdText('headerHeight', value);
+    headerHeight = value;
   };
 
   const setGroupHeaderHeight = (value?: number) => {
     api.setGroupHeaderHeight(value);
-    setIdText('groupHeaderHeight', value);
+    groupHeaderHeight = value;
   };
 
   const setFloatingFiltersHeight = (value?: number) => {
     api.setFloatingFiltersHeight(value);
-    setIdText('floatingFiltersHeight', value);
+    floatingFiltersHeight = value;
   };
 
   const setPivotGroupHeaderHeight = (value?: number) => {
     api.setPivotGroupHeaderHeight(value);
-    setIdText('pivotGroupHeaderHeight', value);
+    pivotGroupHeaderHeight = value;
   };
 
   const setPivotHeaderHeight = (value?: number) => {
     api.setPivotHeaderHeight(value);
-    setIdText('pivotHeaderHeight', value);
+    pivotHeaderHeight = value;
   };
 </script>
 
@@ -131,72 +130,48 @@
 <table>
   <tbody>
     <tr>
-      <td>
-        pivot (
-        <span id="pivot">off</span>
-        )
-      </td>
+      <td>pivot ({pivotStatus ? 'on' : 'off'})</td>
       <td>
         <button on:click={setPivotOn}>on</button>
         <button on:click={setPivotOff}>off</button>
       </td>
     </tr>
     <tr>
-      <td>
-        groupHeaderHeight (
-        <span id="groupHeaderHeight">undefined</span>
-        )
-      </td>
+      <td>groupHeaderHeight ({groupHeaderHeight})</td>
       <td>
         <button on:click={() => setGroupHeaderHeight(40)}>40px</button>
         <button on:click={() => setGroupHeaderHeight(60)}>60px</button>
         <button on:click={() => setGroupHeaderHeight(undefined)}>undefined</button>
       </td>
-      <td>
-        headerHeight (
-        <span id="headerHeight">undefined</span>
-        )
-      </td>
+      <td>headerHeight ({headerHeight})</td>
       <td>
         <button on:click={() => setHeaderHeight(70)}>70px</button>
         <button on:click={() => setHeaderHeight(80)}>80px</button>
         <button on:click={() => setHeaderHeight(undefined)}>undefined</button>
       </td>
     </tr>
-    <tr id="requiresPivot" class="hidden">
-      <td>
-        {' '}
-        pivotGroupHeaderHeight (
-        <span id="pivotGroupHeaderHeight">undefined</span>
-        )
-      </td>
-      <td>
-        <button on:click={() => setPivotGroupHeaderHeight(50)}>50px</button>
-        <button on:click={() => setPivotGroupHeaderHeight(70)}>70px</button>
-        <button on:click={() => setPivotGroupHeaderHeight(undefined)}>undefined</button>
-      </td>
-      <td>
-        pivotHeaderHeight (
-        <span id="pivotHeaderHeight">undefined</span>
-        )
-      </td>
-      <td>
-        <button on:click={() => setPivotHeaderHeight(60)}>60px</button>
-        <button on:click={() => setPivotHeaderHeight(80)}>80px</button>
-        <button on:click={() => setPivotHeaderHeight(undefined)}>undefined</button>
-      </td>
-    </tr>
-    <tr id="requiresNotPivot">
-      <td>
-        floatingFiltersHeight (
-        <span id="floatingFiltersHeight">undefined</span>
-        )
-      </td>
-      <td>
-        <button on:click={() => setFloatingFiltersHeight(35)}>35px</button>
-        <button on:click={() => setFloatingFiltersHeight(55)}>55px</button>
-        <button on:click={() => setFloatingFiltersHeight(undefined)}>undefined</button>
-      </td>
+    <tr>
+      {#if pivotStatus}
+        <td>pivotGroupHeaderHeight ({pivotGroupHeaderHeight})</td>
+        <td>
+          <button on:click={() => setPivotGroupHeaderHeight(50)}>50px</button>
+          <button on:click={() => setPivotGroupHeaderHeight(70)}>70px</button>
+          <button on:click={() => setPivotGroupHeaderHeight(undefined)}>undefined</button>
+        </td>
+        <td>pivotHeaderHeight ({pivotHeaderHeight})</td>
+        <td>
+          <button on:click={() => setPivotHeaderHeight(60)}>60px</button>
+          <button on:click={() => setPivotHeaderHeight(80)}>80px</button>
+          <button on:click={() => setPivotHeaderHeight(undefined)}>undefined</button>
+        </td>
+      {:else}
+        <td>floatingFiltersHeight ({floatingFiltersHeight})</td>
+        <td>
+          <button on:click={() => setFloatingFiltersHeight(35)}>35px</button>
+          <button on:click={() => setFloatingFiltersHeight(55)}>55px</button>
+          <button on:click={() => setFloatingFiltersHeight(undefined)}>undefined</button>
+        </td>
+      {/if}
     </tr>
   </tbody>
 </table>
